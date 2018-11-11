ae978635d58fe783e7e0da54302dc1d06ea38adb
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-11 14:54:53 +0800

    submit form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  @ViewChild('f') form;
   constructor() {}
 
   ngOnInit() {}
+
+  submitForm() {
+    console.log('submit form');
+    console.log(this.form);
+  }
 }
