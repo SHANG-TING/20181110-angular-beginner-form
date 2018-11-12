19606339d9477c05bc817cac6c6c033fdce534ef
SHANG-TING, SIE <rebom8888@gmail.com>
2018-11-12 08:56:34 +0800

    透過 formControl 的 valueChange 來連動資料

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,29 +1,58 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { from, Observable, Subject } from 'rxjs';
+import { concatMap, map, shareReplay, takeUntil, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, AfterViewInit, OnDestroy {
+  private destroy$ = new Subject<any>();
   @ViewChild('f') form;
-  cityTownAreaSource: any;
+  @ViewChild('send') send: FormControl;
+  @ViewChild('selectedCity') selectedCity: FormControl;
+  cityTownAreaSource: Observable<any> = this.http
+    .get('/assets/data/cityarea.json')
+    .pipe(shareReplay(1));
+
+  citys: Observable<Array<string>> = this.cityTownAreaSource.pipe(map(x => Object.keys(x)));
+  areas: Observable<any> = from([]);
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.loadData();
-  }
+  ngOnInit() {}
 
-  loadData() {
-    this.http.get('/assets/data/cityarea.json').subscribe(data => {
-      console.log(data);
-      this.cityTownAreaSource = data;
-    });
+  ngAfterViewInit(): void {
+    this.send.valueChanges
+      .pipe(
+        tap(isSend => {
+          if (!isSend) {
+            return;
+          }
+          setTimeout(() => {
+            this.areas = this.selectedCity.valueChanges.pipe(
+              concatMap(cityName =>
+                this.cityTownAreaSource.pipe(map(x => Object.keys(x[cityName] ? x[cityName] : {}))),
+              ),
+            );
+          }, 0);
+          // kevin大 提示上方的動作可以透過defer實現
+          // defer(() => {}).subscribe();
+        }),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
   }
 
   submitForm() {
     console.log('submit form');
     console.log(this.form);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
